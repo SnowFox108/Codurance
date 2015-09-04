ff509a4721eddea9af2b73f586a95b6867490ff7
SnowFox108 <winsource6@hotmail.com>
2015-09-04 03:06:24 +0100

    init draft

--- Codurance/CrossCutting/Model/Subscription.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace CrossCutting.Model
-{
-    public class Subscription
-    {
-        public User Follower { get; set; }
-        public User Followee { get; set; }
-    }
-}
--- Codurance/CrossCutting/Model/User.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CrossCutting.Model
-{
-    public class User
-    {
-        public string Name { get; set; }
-    }
-}
