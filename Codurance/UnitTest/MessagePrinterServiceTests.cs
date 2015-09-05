2db9196d03df67a0fa4066a5041e887bf5669679
SnowFox108 <winsource6@hotmail.com>
2015-09-05 23:47:29 +0100

    two sample unit tests for demo only

---
+++ Codurance/UnitTest/MessagePrinterServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Codurance.Data.Model;
+using Codurance.Infrastructure;
+using Codurance.Services.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ploeh.AutoFixture;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class MessagePrinterServiceTests
+    {
+        [TestMethod]
+        public void CanPrintMessage()
+        {
+            var fixture = new Fixture();
+            var messages = fixture.CreateMany<Message>(5);
+
+            var mockDateTimeHelper = new Mock<IDateTimeHelper>();
+            var mockPrinterHelper = new Mock<IPrinterHelper>();
+
+            mockDateTimeHelper.Setup(d => d.CurrentDateTime).Returns(DateTime.UtcNow);
+
+            var messagePrinterService = new MessagePrinterService(mockDateTimeHelper.Object, mockPrinterHelper.Object);
+
+            messagePrinterService.PrintMessage(messages);
+
+            mockPrinterHelper.Verify(p => p.WriteLine(It.IsAny<string>()), Times.Exactly(5));
+        }
+
+        [TestMethod]
+        public void CanPrintMessageDescByDate()
+        {
+            var fixture = new Fixture();
+            var messages = fixture.CreateMany<Message>(5);
+            var results = new List<string>();
+
+            var mockDateTimeHelper = new Mock<IDateTimeHelper>();
+            var mockPrinterHelper = new Mock<IPrinterHelper>();
+
+            mockDateTimeHelper.Setup(d => d.CurrentDateTime).Returns(DateTime.UtcNow);
+            mockPrinterHelper.Setup(p => p.WriteLine(It.IsAny<string>())).Callback<string>(s => results.Add(s));
+
+            var messagePrinterService = new MessagePrinterService(mockDateTimeHelper.Object, mockPrinterHelper.Object);
+
+            messagePrinterService.PrintMessage(messages);
+
+
+            Assert.IsTrue(results[0].Contains(messages.OrderByDescending(m => m.CreatedDate).ElementAt(0).Text));
+            Assert.IsTrue(results[1].Contains(messages.OrderByDescending(m => m.CreatedDate).ElementAt(1).Text));
+            Assert.IsTrue(results[2].Contains(messages.OrderByDescending(m => m.CreatedDate).ElementAt(2).Text));
+            Assert.IsTrue(results[3].Contains(messages.OrderByDescending(m => m.CreatedDate).ElementAt(3).Text));
+            Assert.IsTrue(results[4].Contains(messages.OrderByDescending(m => m.CreatedDate).ElementAt(4).Text));
+
+        }
+    }
+}
---
+++ Codurance/UnitTest/MessageServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Codurance.Data.Model;
+using Codurance.Data.Repository;
+using Codurance.Infrastructure;
+using Codurance.Services;
+using Codurance.Services.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ploeh.AutoFixture;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class MessageServiceTests
+    {
+
+        [TestMethod]
+        public void CanPostMessage()
+        {
+            var fixture = new Fixture();
+            var mockUser = fixture.Create<User>();
+
+            var mockMessagePrinterService = new Mock<IMessagePrinterService>();            
+            var mockUserService = new Mock<IUserService>();
+            var mockMessageRepository = new Mock<IMessageRepository>();
+            var mockSubscriptionRepository = new Mock<ISubscriptionRepository>();
+            var mockDateTimeHelper = new Mock<IDateTimeHelper>();
+
+            mockUserService.Setup(u => u.GetUser(It.IsAny<string>())).Returns(mockUser);
+
+            var messageService = new MessageService(mockMessagePrinterService.Object,
+                mockUserService.Object,
+                mockMessageRepository.Object,
+                mockSubscriptionRepository.Object);
+
+            messageService.PostMessage(fixture.Create<string>(), fixture.Create<string>(), mockDateTimeHelper.Object);
+
+            mockMessageRepository.Verify(m => m.CreateMessage(mockUser, It.IsAny<string>(), mockDateTimeHelper.Object), Times.Once);
+        }
+    }
+}
